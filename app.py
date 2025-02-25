5459c8b34f1ef67b5b784cf4f1b430b0626d0b9c
sunny-ksh <rlatjsgnl123@kyonggi.ac.kr>
2025-02-25 17:32:24 +0900

    Changed to bootstrap5

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 # API target:
 # https://api.dictionaryapi.dev/api/v2/entries/en/hello
 from flask import Flask, render_template, request, url_for, redirect
-from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap5
 import requests
 import json, os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
-bootstrap = Bootstrap(app)
+bootstrap = Bootstrap5(app)
 
 @app.route("/", methods=["POST", "GET"])
 def index():
     if request.method == "POST":
         if len(request.form["word"]) > 0:
-            word1 = request.form["word"]
-            return redirect(url_for("find", word=word1), 301)
+            word = request.form["word"]
+            return redirect(url_for("find", word=word), 301)
     return render_template("index.html")
 
 @app.route("/find/", methods=["POST", "GET"])
@@ -26,6 +29,14 @@ def find():
 
     return render_template("find.html", result = result,
                            current_word=word)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 # if __name__ == '__main__':
 #     app.run()
